a3404acba90dd37a5e0d7a478f5862c1eb9509ad
dr3san <andres.sakk@hotmail.com>
2025-08-29 12:01:10 +0300

    Add aliases

--- dot_config/aliases.sh
+++ dot_config/aliases.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 alias l='ls -l --color=auto'
 alias ls='ls --color=auto'
 alias ll='ls -la --color=auto'
@@ -13,9 +15,12 @@ alias chgrp='chgrp --preserve-root'
 alias printenv='printenv | bat -l bash --style="numbers"'
 
 alias make='make -j $(nproc)'
+alias cbuild='rm -rf build && mkdir build && cd build && cmake .. && make && make install'
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 alias gs='git status'
 alias gl='git log --oneline -n 20'
 alias gd='git diff'
 alias ga='git add'
 alias gfixup='git commit --fixup'
+
